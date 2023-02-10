a12f71902fd9d4a529415d9c92ef368a2b3f811c
Ardilos <lic.poletti@gmail.com>
2023-02-10 11:47:41 -0300

    modified:   app.py

--- app.py
+++ app.py
@@ -131,12 +131,13 @@ webrtc_streamer(
 )
 
 
-#clases =('person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush')
 
-#st.table(clases)
 
-#st.write("The full github code is [here](https://github.com/PabloPoletti/prueba_deteccion)")
+
+st.write("List of the 80 different classes that this pre-trained model detects")
 
 image = Image.open('Coco_Classes.png')
 
 st.image(image, caption='COCO Classes')
+
+st.write("The full github code is [here](https://github.com/PabloPoletti/prueba_deteccion)")
